f0280dc37e9c5accaf8bcb9b313b640d35665990
Thiago Bignotto <bignotto@gmail.com>
2024-06-26 05:31:16 -0300

    add dotenv and env file

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
+import "dotenv/config";
 import Fastify from "fastify";
 
 const fastify = Fastify();
+
+console.log(process.env);
 
 fastify.get("/", () => {
   return `The app name is: ${process.env.THE_APP_NAME}`;
 });
 
 try {
-  fastify.listen({ port: 3333 }).then(() => console.log("server running"));
+  fastify
+    .listen({ port: 3333 })
+    .then(() => console.log(`The app name is: ${process.env.THE_APP_NAME}`));
 } catch (error) {
   fastify.log.error(error);
   process.exit(1);
