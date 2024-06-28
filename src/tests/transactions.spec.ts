67ab5dd7e899e6257161959864490a1e7c4aa23a
Thiago Bignotto <bignotto@gmail.com>
2024-06-28 06:43:04 -0300

    :sparkles: configure test environment and knex migrations for tests

--- src/tests/transactions.spec.ts
+++ src/tests/transactions.spec.ts
@@ -1,10 +1,20 @@
+import { execSync } from "child_process";
 import request from "supertest";
-import { beforeAll, describe, expect, it } from "vitest";
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from "vitest";
 import { app } from "../app";
 
 describe("Transaction routes", () => {
   beforeAll(async () => {
     await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  beforeEach(() => {
+    execSync("npm run knex migrate:rollback --all");
+    execSync("npm run knex migrate:latest");
   });
 
   it("should be able to create a new transaction", async () => {
